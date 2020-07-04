aa5317df2c53ca0dcf170a14c242c78b363129dd
muxiaozi <lol_wei@outlook.com>
2020-07-04 22:21:33 +0800

    体重记录提示
    1. 超过100斤红色警告
    2. 比上一次超过1斤，蓝色提示

--- pages/index/index.js
+++ pages/index/index.js
@@ -30,6 +30,7 @@ Page({
   fillList(list) {
     let dayDict = {}
     let result = []
+    let preWeight = -1
     list.forEach(item => {
       let day = util.formatDate(item.date)
       if (!dayDict[day]) {
@@ -40,15 +41,29 @@ Page({
         dayDict[day] = day_item.data
         result.push(day_item)
       }
+
       dayDict[day].push({
         time: util.formatTime(item.date),
-        weight: item.weight
+        weight: item.weight,
+        badge: this.testBadge(preWeight, item.weight),
       })
+      preWeight = item.weight
     });
     this.setData({
       days: result
     })
-    console.log("dayDict:", dayDict)
+  },
+  testBadge(preWeight, curWeight) {
+    // 当前体重大于100斤，显示警告
+    // 体重增幅超过1斤，提示
+    // 其他情况处于安全区域
+    if (curWeight > 100) {
+      return "warn"
+    }
+    if (preWeight != -1 && curWeight - preWeight > 1) {
+      return "info"
+    }
+    return "success"
   },
   record: function() {
     wx.navigateTo({
